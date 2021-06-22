18b073eac355fa269c78829af2e9d6f51b209da1
Garrett Holaday <holadaygarrett@gmail.com>
2021-06-21 20:50:44 -0500

    clean up

---
+++ Assets/Scripts/RaycastWeapon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastWeapon : MonoBehaviour
+{
+    public bool isFiring = false;
+
+    public void StartFiring()
+    {
+        isFiring = true;
+    }
+
+    public void StopFiring()
+    {
+        isFiring = false;
+    }
+}
